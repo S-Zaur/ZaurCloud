0392f13c642e69913a1eefddaf855c7c0c8557e3
My Name <myEmail@example.com>
2023-08-15 13:27:04 +0500

    init Cloud

--- ZaurCloud/settings.py
+++ ZaurCloud/settings.py
@@ -55,6 +55,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
+            os.path.join(BASE_DIR, 'Cloud', 'templates'),
             os.path.join(BASE_DIR, 'templates'),
         ],
         'APP_DIRS': True,
@@ -120,9 +121,12 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'Cloud', 'static'),
 )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/cloud'
--- ZaurCloud/urls.py
+++ ZaurCloud/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cloud/', include("Cloud.urls")),
     path('accounts/', include('django.contrib.auth.urls')),
 ]

0c4de9bbf79aee3cdb86574901ab9f1805a41189
S-Zaur <zaur.safiullin@gmail.com>
2023-09-27 19:13:09 +0500

    Pokemons

--- Pokemons/views.py
+++ Pokemons/views.py
@@ -9,11 +9,9 @@ import json
 def index(request):
     if "name" in request.GET:
         response = r.get('https://pokeapi.co/api/v2/pokemon/' + request.GET["name"])
-        if response.status_code== 404:
-            return Http404
-        print(response.json())
+        if response.status_code == 404:
+            raise Http404
         return render(request, 'Pokemons/index.html', context={"poke": True, "result": response.json()})
     payload = {'limit': 100, 'offset': 0}
     response = r.get('https://pokeapi.co/api/v2/pokemon', params=payload)
-    context = {"objects": json.dumps(response.json()['results'])}
     return render(request, 'Pokemons/index.html', context={"objects": response.json()})

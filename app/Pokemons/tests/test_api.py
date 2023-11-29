d1998ceeafc0b266b5b67b49e4f2ea4b70944f36
S-Zaur <zaur.safiullin@gmail.com>
2023-11-29 17:15:03 +0500

    debug tests

--- app/Pokemons/tests/test_api.py
+++ app/Pokemons/tests/test_api.py
@@ -25,28 +25,29 @@ class MyTestCase(TestCase):
         self.fight = {'player_pokemon': {'id': 3, 'name': 'venusaur', 'hp': 80, 'attack': 82, 'defense': 83,
                                          'base_experience': 263, 'height': 20, 'weight': 1000, 'species': 'venusaur'},
                       'opponent_pokemon': {'id': 4, 'name': 'charmander', 'hp': 39, 'attack': 52, 'defense': 43,
-                                           'base_experience': 62, 'height': 6, 'weight': 85, 'species': 'charmander'}}
+                                           'base_experience': 62, 'height': 6, 'weight': 85, 'species': 'charmander'},
+                      'round_count': 0}
         self.fight_log = [{'player_pokemon': {'id': 3, 'name': 'venusaur', 'hp': 75, 'attack': 82, 'defense': 83,
                                               'base_experience': 263, 'height': 20, 'weight': 1000,
                                               'species': 'venusaur'},
                            'opponent_pokemon': {'id': 4, 'name': 'charmander', 'hp': 39, 'attack': 52, 'defense': 43,
                                                 'base_experience': 62, 'height': 6, 'weight': 85,
                                                 'species': 'charmander'},
-                           'description': 'charmander бьет venusaur и наносит 5 урона'},
+                           'description': 'charmander бьет venusaur и наносит 5 урона', 'round_count': 1},
                           {'player_pokemon': {'id': 3, 'name': 'venusaur', 'hp': 73, 'attack': 82, 'defense': 83,
                                               'base_experience': 263, 'height': 20, 'weight': 1000,
                                               'species': 'venusaur'},
                            'opponent_pokemon': {'id': 4, 'name': 'charmander', 'hp': 39, 'attack': 52, 'defense': 43,
                                                 'base_experience': 62, 'height': 6, 'weight': 85,
                                                 'species': 'charmander'},
-                           'description': 'charmander бьет venusaur и наносит 2 урона'},
+                           'description': 'charmander бьет venusaur и наносит 2 урона', 'round_count': 2},
                           {'player_pokemon': {'id': 3, 'name': 'venusaur', 'hp': 73, 'attack': 82, 'defense': 83,
                                               'base_experience': 263, 'height': 20, 'weight': 1000,
                                               'species': 'venusaur'},
                            'opponent_pokemon': {'id': 4, 'name': 'charmander', 'hp': 0, 'attack': 52, 'defense': 43,
                                                 'base_experience': 62, 'height': 6, 'weight': 85,
                                                 'species': 'charmander'},
-                           'description': 'venusaur бьет charmander и наносит 52 урона'},
+                           'description': 'venusaur бьет charmander и наносит 52 урона', 'round_count': 3},
                           ]
         self.fast_fight_log = {'player_pokemon': {'id': 3, 'name': 'venusaur', 'hp': 67, 'attack': 82, 'defense': 83,
                                                   'base_experience': 263, 'height': 20, 'weight': 1000,
@@ -94,14 +95,16 @@ class MyTestCase(TestCase):
         self.assertEqual(response.json(), self.fight)
         self.fight['player_pokemon'] = str(self.fight['player_pokemon'])
         self.fight['opponent_pokemon'] = str(self.fight['opponent_pokemon'])
+        count = FightResult.objects.all().count()
         for f in self.fight_log:
             response = self.client.post(reverse('Pokemons.API.FightInput', args=[1]), self.fight)
             self.assertEqual(response.status_code, 200)
             self.assertEqual(response.json(), f)
             self.fight['player_pokemon'] = str(response.json()['player_pokemon'])
             self.fight['opponent_pokemon'] = str(response.json()['opponent_pokemon'])
-        res = FightResult.objects.all().first()
-        self.assertEqual(FightResult.objects.all().count(), 1)
+            self.fight['round_count'] = str(response.json()['round_count'])
+        res = FightResult.objects.all().last()
+        self.assertEqual(FightResult.objects.all().count(), count + 1)
         self.assertEqual(res.player_pokemon, 'venusaur')
         self.assertEqual(res.opponent_pokemon, 'charmander')
         self.assertTrue(res.result)
@@ -113,11 +116,12 @@ class MyTestCase(TestCase):
         self.assertEqual(response.json(), self.fight)
         self.fight['player_pokemon'] = str(self.fight['player_pokemon'])
         self.fight['opponent_pokemon'] = str(self.fight['opponent_pokemon'])
+        count = FightResult.objects.all().count()
         response = self.client.get(reverse('Pokemons.API.FastFight'), self.fight)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), self.fast_fight_log)
-        res = FightResult.objects.all().first()
-        self.assertEqual(FightResult.objects.all().count(), 1)
+        res = FightResult.objects.all().last()
+        self.assertEqual(FightResult.objects.all().count(), count + 1)
         self.assertEqual(res.player_pokemon, 'venusaur')
         self.assertEqual(res.opponent_pokemon, 'charmander')
         self.assertTrue(res.result)
